bcd25c4c216e519c8f55885e3d77fe8fb93720ef
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-06-17 00:16:37 -0700

    solve last

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,11 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    var index = array.length - n;
+    if(index < 0) {
+      index = 0;
+    }
+    return n === undefined ? array[array.length -1] : array.slice(index, array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
