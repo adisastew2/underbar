ee4d33c6aced21c10310c7ac8b832dcdc20ec76d
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-06-19 13:00:02 -0700

    solve defaults

--- src/underbar.js
+++ src/underbar.js
@@ -244,11 +244,30 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var extenders = Array.prototype.slice.call(arguments, 1);
+
+    _.each(extenders, (item)=>{
+      for(var key in item){
+        obj[key] = item[key];
+      }
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    var extenders = Array.prototype.slice.call(arguments, 1);
+
+    _.each(extenders, (item)=>{
+      for(var key in item){
+        if(obj[key] === undefined){
+          obj[key] = item[key];
+        }
+        
+      }
+    });
+    return obj;
   };
 
 
